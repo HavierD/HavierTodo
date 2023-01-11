66d1b66c5986158cdd7cb1c9a89dd5ba4b339c0f
HavierDai <huaizhi.dai@nre.tas.gov.au>
2023-01-11 15:13:51 +1100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hvtd-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'havier-todo';
+}
